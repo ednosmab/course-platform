03a0cd431ab35298e678aa8090797730b636cb61
Edson Garcia <ednosmab@yahoo.com.br>
2026-05-19 02:58:24 -0300

    fix(student): add @tamagui/babel-plugin to transform styled components
    Without the babel plugin, Tamagui styled() components may not render
    correctly on web. The plugin compiles createTamagui config and
    optimizes styled() calls for proper CSS generation.

--- apps/student/babel.config.js
+++ apps/student/babel.config.js
@@ -2,5 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['@tamagui/babel-plugin', {
+        components: ['tamagui'],
+        config: './node_modules/@projeto/ui/src/tamagui.config.ts',
+      }],
+    ],
   };
 };
