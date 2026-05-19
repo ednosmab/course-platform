460da84d421eba8ca34caf0b07e90e68bfdc11c2
Edson Garcia <ednosmab@yahoo.com.br>
2026-05-19 02:38:18 -0300

    fix(student): configure @rnx-kit/metro-resolver-symlinks to fix Metro + pnpm resolution
    The student app was showing a blank screen because Metro couldn't resolve
    'react' from pnpm's virtual store (.pnpm). Added @rnx-kit/metro-resolver-symlinks
    as the resolveRequest in metro.config.js to handle symlinks correctly.

--- apps/student/metro.config.js
+++ apps/student/metro.config.js
@@ -1,5 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
+const makeMetroResolver = require('@rnx-kit/metro-resolver-symlinks');
 
 const config = getDefaultConfig(__dirname);
 
@@ -8,6 +9,7 @@ config.watchFolders = [
   path.resolve(__dirname, '../../node_modules'),
 ];
 
+config.resolver.resolveRequest = makeMetroResolver();
 config.resolver.unstable_enablePackageExports = true;
 
 module.exports = config;
