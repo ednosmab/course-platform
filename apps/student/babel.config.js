1de6ebf19f898e63c44e3b86feaa1c516a4caf41
Edson Garcia <ednosmab@yahoo.com.br>
2026-05-19 02:20:21 -0300

    fix(student): remove misconfigured tamagui babel plugin

--- apps/student/babel.config.js
+++ apps/student/babel.config.js
@@ -2,14 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        '@tamagui/babel-plugin',
-        {
-          config: './tsconfig.json',
-          components: ['tamagui'],
-        },
-      ],
-    ],
   };
 };
