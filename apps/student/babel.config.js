23b4f95c0d11ee69227a26af3b40cfbf31520a5c
Edson Garcia <ednosmab@yahoo.com.br>
2026-05-19 02:16:59 -0300

    fix(student): add metro.config.js for monorepo, add tamagui babel plugin, remove type:module

--- apps/student/babel.config.js
+++ apps/student/babel.config.js
@@ -2,5 +2,14 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        '@tamagui/babel-plugin',
+        {
+          config: './tsconfig.json',
+          components: ['tamagui'],
+        },
+      ],
+    ],
   };
 };
