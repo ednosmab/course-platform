552acf2ff459f68ba93ee93fe90a7c2eec2bee1f
Edson Garcia <ednosmab@yahoo.com.br>
2026-05-19 11:42:38 -0300

    fix: hide sidebars in preview mode for full-width canvas

--- apps/admin/src/app/studio/[courseId]/page.tsx
+++ apps/admin/src/app/studio/[courseId]/page.tsx
@@ -3,22 +3,31 @@
 import React from 'react';
 import { YStack, XStack } from '@projeto/ui';
 import { EditorProvider } from '../../../context/EditorContext';
+import { useEditor } from '../../../context/EditorContext';
 import { EditorHeader } from '../../../components/editor/EditorHeader';
 import { BlockPalette } from '../../../components/editor/BlockPalette';
 import { EditorCanvas } from '../../../components/editor/EditorCanvas';
 import { BlockSettings } from '../../../components/editor/BlockSettings';
 
+function StudioLayout() {
+  const { previewMode } = useEditor();
+
+  return (
+    <YStack f={1} h="100vh" w="100vw" overflow="hidden">
+      <EditorHeader />
+      <XStack f={1} overflow="hidden" w="100%">
+        {!previewMode && <BlockPalette />}
+        <EditorCanvas />
+        {!previewMode && <BlockSettings />}
+      </XStack>
+    </YStack>
+  );
+}
+
 export default function StudioPage({ params }: { params: { courseId: string } }) {
   return (
     <EditorProvider>
-      <YStack f={1} h="100vh" w="100vw" overflow="hidden">
-        <EditorHeader />
-        <XStack f={1} overflow="hidden" w="100%">
-          <BlockPalette />
-          <EditorCanvas />
-          <BlockSettings />
-        </XStack>
-      </YStack>
+      <StudioLayout />
     </EditorProvider>
   );
 }
